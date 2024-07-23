7f7b81b90982594a7cec30c102ec8b518673e4a2
Kirill <syskirill.com>
2024-07-23 14:36:56 +0300

    timer label works

--- Colorize/ViewController.swift
+++ Colorize/ViewController.swift
@@ -17,40 +17,58 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var timerLabel: UILabel!
     
-    var timer : Timer?
+    var timer5 : Timer?
+    
+    var timer1 : Timer?
+    
+    var seconds = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateLabel()
         redButton.addTarget(self, action: #selector(redButtonPressed), for: .touchUpInside)
         greenButton.addTarget(self, action: #selector(greenButtonPressed), for: .touchUpInside)
         blueButton.addTarget(self, action: #selector(blueButtonPressed), for: .touchUpInside)
     }
     
     func makeTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(timerFired), userInfo: nil, repeats: false)
+        timer1 = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timer1Fired), userInfo: nil, repeats: true)
+        timer5 = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(timerFired), userInfo: nil, repeats: false)
+    }
+    
+    func updateLabel() {
+        timerLabel.text = "\(seconds)"
     }
     
     @objc func timerFired() {
+        seconds = 5
+        updateLabel()
         view.backgroundColor = .systemGray3
-        timer?.invalidate()
+        timer5?.invalidate()
+        timer1?.invalidate()
     }
     
     @objc func redButtonPressed() {
-        timer?.invalidate()
+        timer5?.invalidate()
         view.backgroundColor = .red
         makeTimer()
     }
     
     @objc func greenButtonPressed() {
-        timer?.invalidate()
+        timer5?.invalidate()
         view.backgroundColor = .green
         makeTimer()
     }
     
     @objc func blueButtonPressed() {
-        timer?.invalidate()
+        timer5?.invalidate()
         view.backgroundColor = .blue
         makeTimer()
+    }
+    
+    @objc func timer1Fired() {
+        seconds -= 1
+        updateLabel()
     }
 }
 
